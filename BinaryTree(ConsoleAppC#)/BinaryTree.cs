95a1e4347dbee1efcfab36fe284c26cf408c0550
ZexaZexaDubrovskiy <ZexaZexaDubrovskiy@mail.ru>
2024-09-09 13:46:03 +0300

    feat(program): add binary tree traversal project

---
+++ BinaryTree(ConsoleAppC#)/BinaryTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class BinaryTree
+{
+    public TreeNode Root;
+
+    public BinaryTree(int rootValue) =>
+        Root = new TreeNode(rootValue);
+
+    public void PreOrderTraversal(TreeNode node)
+    {
+        if (node == null) return;
+
+        Console.Write(node.Value + " ");
+        PreOrderTraversal(node.Left);
+        PreOrderTraversal(node.Right);
+    }
+
+    public void InOrderTraversal(TreeNode node)
+    {
+        if (node == null) return;
+
+        InOrderTraversal(node.Left);
+        Console.Write(node.Value + " ");
+        InOrderTraversal(node.Right);
+    }
+
+    public void PostOrderTraversal(TreeNode node)
+    {
+        if (node == null) return;
+
+        PostOrderTraversal(node.Left);
+        PostOrderTraversal(node.Right);
+        Console.Write(node.Value + " ");
+    }
+}
---
+++ BinaryTree(ConsoleAppC#)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        BinaryTree tree = new BinaryTree(1);
+        tree.Root.Left = new TreeNode(2);
+        tree.Root.Right = new TreeNode(3);
+        tree.Root.Left.Left = new TreeNode(4);
+        tree.Root.Left.Right = new TreeNode(5);
+        tree.Root.Right.Left = new TreeNode(6);
+        tree.Root.Right.Right = new TreeNode(7);
+
+        Console.WriteLine("Pre-order:");
+        tree.PreOrderTraversal(tree.Root);
+        Console.WriteLine();
+
+        Console.WriteLine("In-order:");
+        tree.InOrderTraversal(tree.Root);
+        Console.WriteLine();
+
+        Console.WriteLine("Post-order:");
+        tree.PostOrderTraversal(tree.Root);
+        Console.WriteLine();
+    }
+}
